8ee4d638199e64d5dfc4a5568ff35a95fc14d6cd
Ivan <Ivan Filonenko>
2019-11-02 01:53:48 +0300

    Scripted auto-commit on change (2019-11-02 01:53:48) by gitwatch.sh

--- src/main/java/lab4/SolveTestActor.java
+++ src/main/java/lab4/SolveTestActor.java
@@ -29,7 +29,7 @@ public class SolveTestActor extends AbstractActor {
             }
             params = params.substring(0, params.length()-1);
 
-            String res = invocable.invokeFunction(pack.getFunctionName(), params).toString();
+            String res = invocable.invokeFunction(pack.getFunctionName(), Object[] pack.getTests()[index].getParams()).toString();
             pack.writeResult(index, res);
             System.out.println(res);
             getSender().tell(pack, ActorRef.noSender());
