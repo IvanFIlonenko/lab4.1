6a206652310863ee026923180f5cdb9d270e8944
Ivan <Ivan Filonenko>
2019-11-02 02:16:01 +0300

    Scripted auto-commit on change (2019-11-02 02:16:01) by gitwatch.sh

--- src/main/java/lab4/Main.java
+++ src/main/java/lab4/Main.java
@@ -1,7 +1,3 @@
-/*
- * Copyright (C) 2009-2019 Lightbend Inc. <https://www.lightbend.com>
- */
-
 package lab4;
 
 import akka.NotUsed;
@@ -10,41 +6,24 @@ import akka.actor.ActorSystem;
 import akka.actor.Props;
 import akka.http.javadsl.ConnectHttp;
 import akka.http.javadsl.Http;
-//#imports
 import akka.http.javadsl.ServerBinding;
 import akka.http.javadsl.marshallers.jackson.Jackson;
 import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
-import akka.http.javadsl.model.StatusCodes;
-//#imports
 import akka.http.javadsl.server.AllDirectives;
 import akka.http.javadsl.server.Route;
 import akka.pattern.Patterns;
 import akka.stream.ActorMaterializer;
 import akka.stream.javadsl.Flow;
 import scala.concurrent.Future;
-
-import java.io.IOException;
-//#imports
-import java.util.Map;
-import java.util.concurrent.CompletableFuture;
-//#imports
 import java.util.concurrent.CompletionStage;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.function.Function;
 
-//#imports
-import static akka.http.javadsl.server.Directives.*;
-import static akka.http.javadsl.unmarshalling.StringUnmarshallers.INTEGER;
-
-//#imports
 
 public class Main extends AllDirectives {
 
     static ActorRef manager;
 
 
-    //#unmarshall
     private Route appRoute() {
         return concat(
                 get(
@@ -73,7 +52,6 @@ public class Main extends AllDirectives {
         final Http http = Http.get(system);
         final ActorMaterializer materializer = ActorMaterializer.create(system);
 
-        //In order to access all directives we need an instance where the routes are define.
         Main app = new Main();
 
         final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = app.appRoute().flow(system, materializer);
@@ -81,10 +59,10 @@ public class Main extends AllDirectives {
                 ConnectHttp.toHost("localhost", 8080), materializer);
 
         System.out.println("Server online at http://localhost:8080/\nPress RETURN to stop...");
-        System.in.read(); // let it run until user presses return
+        System.in.read();
 
         binding
-                .thenCompose(ServerBinding::unbind) // trigger unbinding from the port
-                .thenAccept(unbound -> system.terminate()); // and shutdown when done
+                .thenCompose(ServerBinding::unbind)
+                .thenAccept(unbound -> system.terminate()); 
     }
 }
