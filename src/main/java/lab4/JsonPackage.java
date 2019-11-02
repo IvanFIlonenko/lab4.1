e5e594bfa6114db23b2daa4ee07e2212491b0490
Ivan <Ivan Filonenko>
2019-11-02 10:27:57 +0300

    Scripted auto-commit on change (2019-11-02 10:27:57) by gitwatch.sh

--- src/main/java/lab4/JsonPackage.java
+++ src/main/java/lab4/JsonPackage.java
@@ -5,16 +5,20 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 
 public class JsonPackage {
+    private static final String PACKAGE_ID = "packageId";
+    private static final String JS_SCRIPT = "jsScript";
+    private static final String FUNCTION_NAME = "functionName";
+    private static final String TESTS = "tests";
     private final int packageId;
     private final String jsScript;
     private final String functionName;
     private final Test[] tests;
 
     @JsonCreator
-    public JsonPackage(@JsonProperty("packageId") String packageId,
-                       @JsonProperty("jsScript") String jsScript,
-                       @JsonProperty("functionName") String functionName,
-                       @JsonProperty("tests") Test[] tests) {
+    public JsonPackage(@JsonProperty(PACKAGE_ID) String packageId,
+                       @JsonProperty(JS_SCRIPT) String jsScript,
+                       @JsonProperty(FUNCTION_NAME) String functionName,
+                       @JsonProperty(TESTS) Test[] tests) {
         this.packageId = Integer.parseInt(packageId);
         this.jsScript= jsScript;
         this.functionName = functionName;
